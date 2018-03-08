5a586e946668bfca0669342be197339240328937
Filip <filip.ingemarsson@live.com>
2018-03-08 10:19:14 +0100

    just to save minor things

--- Js/main.js
+++ Js/main.js
@@ -1,42 +1,69 @@
 var url = `https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/`;
 var mykey = apiKey.MY_KEY;
-var person1Name = "ziper240";
-var person1Platform = "pc";
-var person2Name = "OlstedT";
-var person2Platform = "pc";
 var bothPlayersArray = [];
 
-function fetchStats(){
+document.getElementById('compareButton').addEventListener('click', getTwoPlayersAndURL);
+
+function getTwoPlayersAndURL(){
     
-  var comparePlayers = [];
+    const player1Name = document.getElementById('player1Name').value;
+    const player1Platform = document.getElementById("player1Platform").value;
+    const player1URL = `${url}${player1Platform}/${player1Name}`;
     
-  const player1 = fetch(`https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/pc/ziper240`, {
-    headers: { 'TRN-Api-Key': mykey }
-  }).then((response) => response.json())
+    const player2Name = document.getElementById('player2Name').value;
+    const player2Platform = document.getElementById("player2Platform").value;
+    const player2URL = `${url}${player2Platform}/${player2Name}`;   
     
+    fetchStats(player1URL, player2URL)
+}
+
+function fetchStats(player1URL, player2URL) {
+    
+    var comparePlayers = [];
+
+    const player1 = fetch(player1URL, {
+        headers: {'TRN-Api-Key': mykey}
+    }).then((response) => response.json())
+
     comparePlayers.push(player1);
-    
-  const player2 = fetch(`https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/profile/pc/OlstedT`, {
-    headers: { 'TRN-Api-Key': mykey }
-  }).then((response) => response.json())
-    
+
+    const player2 = fetch(player2URL, {
+        headers: {'TRN-Api-Key': mykey }
+    }).then((response) => response.json())
+
     comparePlayers.push(player2);
 
     Promise.all(comparePlayers)
         .then((bothPlayers) => {
             bothPlayersArray.push(bothPlayers);
-            //function listPlayerStats();
+            listPlayersLifetimeStats();
             console.log(bothPlayersArray);
         })
 }
 
-fetchStats();
+function listPlayersLifetimeStats(){
 
-function displayPlayer1(fortniteData){
+    const listOfPlayersLifetimeStats = 
+          document.getElementById('listOfPlayersLifetimeStats');
+    const player1 = bothPlayersArray[0][0];
+    const player2 = bothPlayersArray[0][1];
+    console.log(player1);
+    console.log(player2);
     
- const displayFortnite = document.getElementById('FortniteDiv');
+    var FortniteInfo = `
+
+    `;
+    
+    listOfPlayersLifetimeStats.innerHTML += FortniteInfo;
+}
+
+
+/*
+function displayPlayer1(fortniteData) {
+
+    const displayFortnite = document.getElementById('FortniteDiv');
     const lifeTimeStats = fortniteData.lifeTimeStats
-var FortniteInfo = `
+    var FortniteInfo = `
     <p> ${fortniteData.epicUserHandle} </p>
     <div class="grid-container">
     `;
@@ -48,7 +75,7 @@ var FortniteInfo = `
         </div>`;
     }
     FortniteInfo += `</div>`;
-        
+
     displayFortnite.innerHTML += FortniteInfo;
     console.log(compare);
-};
+};*/
