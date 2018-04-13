40bc41e54e05823fe7c9c178fa48a2fb02a9fa33
Filip <filip.ingemarsson@live.com>
2018-04-13 09:53:51 +0200

    fixed bug: api had chaged value

--- Js/main.js
+++ Js/main.js
@@ -3,44 +3,48 @@ var mykey = apiKey.MY_KEY;
 compareButton();
 checkScreenForbackgroundImg();
 
-function compareButton(){
+function compareButton() {
     var compareButton = document.getElementById("compareButton");
-        compareButton.onclick = function() {
-            compareButton.disabled = true;
-            setTimeout(function() {
-                compareButton.disabled = false;
-            }, 5000);
+    compareButton.onclick = function () {
+        compareButton.disabled = true;
+        setTimeout(function () {
+            compareButton.disabled = false;
+        }, 5000);
         getTwoPlayersAndURL();
     };
 }
 //get API for the fetch request from input fields + url
-function getTwoPlayersAndURL(){
-    
+function getTwoPlayersAndURL() {
+
     const player1Name = document.getElementById('player1Name').value;
     const player1Platform = document.getElementById("player1Platform").value;
     const player1URL = `${url}${player1Platform}/${player1Name}`;
-    
+
     const player2Name = document.getElementById('player2Name').value;
     const player2Platform = document.getElementById("player2Platform").value;
-    const player2URL = `${url}${player2Platform}/${player2Name}`;   
-    
+    const player2URL = `${url}${player2Platform}/${player2Name}`;
+
     fetchStats(player1URL, player2URL)
     loadingScreen();
 }
 
-//This function get players from API at the same time 
+//This function get players from API at the same time
 function fetchStats(player1URL, player2URL) {
-    
+
     var comparePlayers = [];
 
     const player1 = fetch(player1URL, {
-        headers: {'TRN-Api-Key': mykey}
-    }).then((response) => response.json())    
+        headers: {
+            'TRN-Api-Key': mykey
+        }
+    }).then((response) => response.json())
 
     comparePlayers.push(player1);
 
     const player2 = fetch(player2URL, {
-        headers: {'TRN-Api-Key': mykey }
+        headers: {
+            'TRN-Api-Key': mykey
+        }
     }).then((response) => response.json())
 
     comparePlayers.push(player2);
@@ -48,54 +52,50 @@ function fetchStats(player1URL, player2URL) {
     Promise.all(comparePlayers)
         .then((bothPlayers) => {
             bothPlayersArray = [];
-        console.log(bothPlayersArray);
             bothPlayersArray.push(bothPlayers);
             listPlayersLifetimeStats();
             comparePLayerStats();
         })
         .catch((error) => {
-            if(error == "SyntaxError: Unexpected token < in JSON at position 0"){
+            if (error == "SyntaxError: Unexpected token < in JSON at position 0") {
                 errorMessage("Please put in value!");
-            }
-            else if(error == "TypeError: Cannot read property '8' of undefined"){
+            } else if (error == "TypeError: Cannot read property '8' of undefined") {
                 errorMessage("Something went wrong please check players and platform!");
-            }
-            else if(error == "TypeError: Cannot read property 'label' of undefined"){
+            } else if (error == "TypeError: Cannot read property 'label' of undefined") {
                 errorMessage("Cannot comapre stats because one or both have missing stats!");
-            }
-            else{
+            } else {
                 errorMessage("Something went wrong!");
             }
         })
 }
 //list lifetime stats of both players
-function listPlayersLifetimeStats(){
-    const containerForPlayersLifetimeStats = 
-          document.getElementById('containerForPlayersLifetimeStats');
-    const sortBy = 
-          document.getElementById('sortBy');
-    
+function listPlayersLifetimeStats() {
+    const containerForPlayersLifetimeStats =
+        document.getElementById('containerForPlayersLifetimeStats');
+    const sortBy =
+        document.getElementById('sortBy');
+
     const player = bothPlayersArray[0];
-    
+
     //add three stats to show top 3-10 the return the number to diplay
-    function sumTop3_10(player){
-        sum = +player.lifeTimeStats[0].value 
-            + +player.lifeTimeStats[1].value 
-            + +player.lifeTimeStats[2].value;
+    function sumTop3_10(player) {
+        sum = +player.lifeTimeStats[0].value +
+            +player.lifeTimeStats[1].value +
+            +player.lifeTimeStats[2].value;
         return sum;
     }
-    
+
     //add three stats to show top 6-25 the return the number to diplay
-    function sumTop6_25(player){
-        sum = +player.lifeTimeStats[3].value 
-            + +player.lifeTimeStats[4].value 
-            + +player.lifeTimeStats[5].value;
+    function sumTop6_25(player) {
+        sum = +player.lifeTimeStats[3].value +
+            +player.lifeTimeStats[4].value +
+            +player.lifeTimeStats[5].value;
         return sum;
     }
-    
+
     let fortniteInfo = '';
     for (let i = 0; i < player.length; i++) {
-    fortniteInfo += `
+        fortniteInfo += `
         <div class="player${[i + 1]}">
             <div class="nameAndPlatform">
                 <span class="name">${player[i].epicUserHandle}</span>
@@ -113,7 +113,7 @@ function listPlayersLifetimeStats(){
                 <div class="lifetimeTop6-25">
                     <p>${sumTop6_25(player[i])}</p>
                     <p>Top6/12/25</p>
-                </div>      
+                </div>
             </div>
             <div class="lifetimeStats">
                 <p>${player[i].lifeTimeStats[6].key}:</p><p>${player[i].lifeTimeStats[6].value}</p>
@@ -121,8 +121,8 @@ function listPlayersLifetimeStats(){
                 <p>${player[i].lifeTimeStats[9].key}:</p><p>${player[i].lifeTimeStats[9].value}</p>
                 <p>${player[i].lifeTimeStats[10].key}:</p><p>${player[i].lifeTimeStats[10].value}</p>
                 <p>${player[i].lifeTimeStats[11].key}:</p><p>${player[i].lifeTimeStats[11].value}</p>
-                <p>${player[i].lifeTimeStats[13].key}:</p><p>${player[i].lifeTimeStats[13].value}</p>
-            </div>   
+                <p>${player[i].lifeTimeStats[12].key}:</p><p>${player[i].lifeTimeStats[12].value}</p>
+            </div>
         </div>
     `;
     }
@@ -137,12 +137,12 @@ function listPlayersLifetimeStats(){
                     <option value="curr_p10">S3 Squad</option>
         </select>
     `;
-    
+
     containerForPlayersLifetimeStats.innerHTML = fortniteInfo;
     sortBy.innerHTML = select;
-    
+
     //check if the select value has changed and send the new value into the display function
-    document.getElementById('sortByGamemode').addEventListener('change', function() {
+    document.getElementById('sortByGamemode').addEventListener('change', function () {
         this.selected = this.value;
         var gamemode = this.value;
         comparePLayerStats(gamemode);
@@ -150,41 +150,41 @@ function listPlayersLifetimeStats(){
 }
 
 //list all the differnt stats that you would like to compare based on select menu above
-function comparePLayerStats(gameMode){
-    const containerForComparePLayerStats = 
-          document.getElementById('containerForComparePLayerStats');
-        var player1 = '';
-        var player2 = '';
-    switch(gameMode) {
+function comparePLayerStats(gameMode) {
+    const containerForComparePLayerStats =
+        document.getElementById('containerForComparePLayerStats');
+    var player1 = '';
+    var player2 = '';
+    switch (gameMode) {
         case "p2":
             player1 = bothPlayersArray[0][0].stats.p2;
             player2 = bothPlayersArray[0][1].stats.p2;
-        break;
+            break;
         case "p9":
-             player1 = bothPlayersArray[0][0].stats.p9;
-             player2 = bothPlayersArray[0][1].stats.p9;
-        break;
+            player1 = bothPlayersArray[0][0].stats.p9;
+            player2 = bothPlayersArray[0][1].stats.p9;
+            break;
         case "p10":
-             player1 = bothPlayersArray[0][0].stats.p10;
-             player2 = bothPlayersArray[0][1].stats.p10;
-        break;
+            player1 = bothPlayersArray[0][0].stats.p10;
+            player2 = bothPlayersArray[0][1].stats.p10;
+            break;
         case "curr_p2":
-             player1 = bothPlayersArray[0][0].stats.curr_p2;
-             player2 = bothPlayersArray[0][1].stats.curr_p2;
-        break;
+            player1 = bothPlayersArray[0][0].stats.curr_p2;
+            player2 = bothPlayersArray[0][1].stats.curr_p2;
+            break;
         case "curr_p9":
-             player1 = bothPlayersArray[0][0].stats.curr_p9;
-             player2 = bothPlayersArray[0][1].stats.curr_p9;
-        break;
+            player1 = bothPlayersArray[0][0].stats.curr_p9;
+            player2 = bothPlayersArray[0][1].stats.curr_p9;
+            break;
         case "curr_p10":
-             player1 = bothPlayersArray[0][0].stats.curr_p10;
-             player2 = bothPlayersArray[0][1].stats.curr_p10;
-        break;
+            player1 = bothPlayersArray[0][0].stats.curr_p10;
+            player2 = bothPlayersArray[0][1].stats.curr_p10;
+            break;
         default:
             player1 = bothPlayersArray[0][0].stats.p2;
             player2 = bothPlayersArray[0][1].stats.p2;
     }
-      
+
     let fortniteInfo = ``;
     fortniteInfo += `
         <div class="comparePlayers">
@@ -221,11 +221,11 @@ function comparePLayerStats(gameMode){
                         <div class="progress" style="width: ${100 - +player1.kd.percentile}%;"></div>
                     </div>
                 </div>
-                <div class="minutesPlayed">
-                    <p>${player1.avgTimePlayed.label}</p>
-                    <p>${player1.avgTimePlayed.displayValue}</p>
+                <div class="scorePerMatch ">
+                    <p>${player1.scorePerMatch.label}</p>
+                    <p>${player1.scorePerMatch.displayValue}</p>
                     <div class="progressbar">
-                        <div class="progress" style="width: ${100 - +player1.avgTimePlayed.percentile}%;"></div>
+                        <div class="progress" style="width: ${100 - +player1.scorePerMatch.percentile}%;"></div>
                     </div>
                 </div>
             </div>
@@ -259,12 +259,12 @@ function comparePLayerStats(gameMode){
                     <p>${player2.kd.label}</p>
                     <p>${player2.kd.displayValue}</p>
                 </div>
-                <div class="minutesPlayed">
+                <div class="scorePerMatch">
                     <div class="progressbar">
-                        <div class="progress" style="width: ${100 - +player2.avgTimePlayed.percentile}%;"></div>
+                        <div class="progress" style="width: ${100 - +player2.scorePerMatch.percentile}%;"></div>
                     </div>
-                    <p>${player2.avgTimePlayed.label}</p>
-                    <p>${player2.avgTimePlayed.displayValue}</p>
+                    <p>${player2.scorePerMatch.label}</p>
+                    <p>${player2.scorePerMatch.displayValue}</p>
                 </div>
             </div>
             <div class="player2compareLabel">
@@ -273,19 +273,19 @@ function comparePLayerStats(gameMode){
             </div>
         </div>
     `;
-    
+
     containerForComparePLayerStats.innerHTML = fortniteInfo;
 }
 
 //Create a spinner icon for when the page is loading
-function loadingScreen(){
-    const containerForPlayersLifetimeStats = 
-          document.getElementById('containerForPlayersLifetimeStats');
-    const containerForComparePLayerStats = 
-          document.getElementById('containerForComparePLayerStats');
-    const sortBy = 
-          document.getElementById('sortBy');
-    
+function loadingScreen() {
+    const containerForPlayersLifetimeStats =
+        document.getElementById('containerForPlayersLifetimeStats');
+    const containerForComparePLayerStats =
+        document.getElementById('containerForComparePLayerStats');
+    const sortBy =
+        document.getElementById('sortBy');
+
     let loading = `
     <div class="loadingSpinner"></div>
     `;
@@ -297,19 +297,18 @@ function loadingScreen(){
 
 //Change background based on screen width and height
 function checkScreenForbackgroundImg() {
-    var w = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
+    var w = window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
 
-    var h = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
-    
-    if(w > h){
+    var h = window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+
+    if (w > h) {
         document.body.classList.add("desktopImg");
-            
-    }
-    else{
+
+    } else {
         document.body.classList.add("mobileImg");
     }
 }
